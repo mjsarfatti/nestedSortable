93e1996fd3f4a1a0e8e661be293a68caf2cfbd08
vsn4ik <vsn4ik@gmail.com>
2013-06-11 09:17:29 +0400

    startDepthCount not used

--- jquery.mjs.nestedSortable.js
+++ jquery.mjs.nestedSortable.js
@@ -450,7 +450,6 @@
 		toHierarchy: function(options) {
 
 			var o = $.extend({}, this.options, options),
-				sDepth = o.startDepthCount || 0,
 			    ret = [];
 
 			$(this.element).children(o.items).each(function () {
